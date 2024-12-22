14cb8582513a686f758424aacc88f8598d594e57
Sufyān <sofiane59pro@gmail.com>
2024-12-22 17:31:10 +0100

    verification si la réponse est vide

--- PileOuFace.NET/Program.cs
+++ PileOuFace.NET/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+bool isValid;
+
+
+
+while (true)
+{
+    Console.Write("Choisis si c'est pile ou face.");
+    string reponseUser = Console.ReadLine();
+
+
+    if (string.IsNullOrEmpty(reponseUser))
+    {
+        Console.WriteLine("Veuillez écrire une réponse");
+    }
+
+
+}
