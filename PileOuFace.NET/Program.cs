fb129939c6ae73a32c87c6c699ab9e7cc0519955
Sufyān <sofiane59pro@gmail.com>
2024-12-22 17:38:37 +0100

    ajout de verification réponse correspondante a la demande

--- PileOuFace.NET/Program.cs
+++ PileOuFace.NET/Program.cs
@@ -5,14 +5,23 @@ bool isValid;
 
 while (true)
 {
-    Console.Write("Choisis si c'est pile ou face.");
+    Console.Write("Choisis si c'est pile ou face:";
     string reponseUser = Console.ReadLine();
 
 
     if (string.IsNullOrEmpty(reponseUser))
     {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Veuillez écrire une réponse");
+        Console.ResetColor();
+    } 
+    else if (reponseUser != "pile" || reponseUser != "PILE" || reponseUser != "face"|| reponseUser != "FACE")
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Vous n'étes pas autorisée a effectuer cette action.");
+        Console.ResetColor();
     }
-
 
 }
