c35c7bb28627a3c5ab35430f43462c221d543c79
Sufyān <sofiane59pro@gmail.com>
2024-12-23 20:43:14 +0100

    reglage problème reponseUserConverted default value

--- PileOuFace.NET/Program.cs
+++ PileOuFace.NET/Program.cs
@@ -1,12 +1,16 @@
 ﻿
 bool isValid;
+// Je donne une valeur de défault avant de lui en assigner une autre, sinons erreur du compileur.
+int reponseUserConverted = 0;
+
 
 
 
 while (true)
 {
-    Console.Write("Choisis si c'est pile ou face:";
+    Console.Write("Choisis si c'est pile ou face:");
     string reponseUser = Console.ReadLine();
+    
 
 
     if (string.IsNullOrEmpty(reponseUser))
@@ -22,6 +26,23 @@ while (true)
         Console.ForegroundColor = ConsoleColor.DarkRed;
         Console.WriteLine("Vous n'étes pas autorisée a effectuer cette action.");
         Console.ResetColor();
+    } else if (reponseUser == "pile" || reponseUser == "PILE")
+    {
+        reponseUserConverted = 1;
     }
+
+    else if (reponseUser == "face" || reponseUser == "FACE")
+    {
+        reponseUserConverted = 2;
+    }
+
+    Random random = new Random();
+    int result = random.Next(1,2);
+
+    if (result == reponseUserConverted)
+    {
+
+    }
+
 
 }
